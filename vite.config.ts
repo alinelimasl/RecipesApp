eb81e2b51531b5aaea6aa90cd8ca1d5fcce9c7ac
Aline <aline_lima@outlook.com>
2023-10-30 22:14:24 -0300

    Update imagens

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   test: {
     globals: true,
     environment: 'jsdom',
@@ -14,13 +15,8 @@ export default defineConfig({
     coverage: {
       reporter: ['text', 'json', 'html'],
       include: ['src/**/*'],
-      exclude: [
-        "src/main.tsx",
-        "src/fakes",
-        "src/routes",
-        "src/tests"
-      ],
-      provider: 'c8'
+      exclude: [],
+      provider: 'istanbul'
     }
   },
 })
